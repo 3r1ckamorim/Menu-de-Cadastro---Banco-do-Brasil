cadcd692f6b374c7d614fadc2fdd7f1b7f5df3e6
erick <3r1ckamorim@gmail.com>
2022-09-01 19:50:50 -0300

    fonte

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models.Home;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Dados(string Nome, int Idade, int Cpf, string EstadoCivil, string NomeDeTratamento, int DocumentoDeIdentidade, string Sexo, string Escolaridade,
+            string FormaçãoAcademica, DateTime ConclucaoMmesAno, string FormacaoAcademicaemAndamento, int CNPJdaEscola, DateTime Previsaodeconcluçao, int DDD, int Telefone, string Email )
+        {
+            DadosViewModel dadosViewModel = new DadosViewModel();
+
+            dadosViewModel.Idade = Idade;
+            dadosViewModel.Nome = Nome;
+            dadosViewModel.EstadoCivil = EstadoCivil;
+            dadosViewModel.Cpf = Cpf;
+            dadosViewModel.EstadoCivil = EstadoCivil;
+            dadosViewModel.NomeDeTratamento = NomeDeTratamento;
+            dadosViewModel.DocumentoDeIdentidade = DocumentoDeIdentidade;
+            dadosViewModel.Sexo = Sexo;
+            dadosViewModel.Escolaridade = Escolaridade;
+            dadosViewModel.FormaçãoAcademica = FormaçãoAcademica;
+            dadosViewModel.ConclucaoMmesAno = ConclucaoMmesAno;
+            dadosViewModel.FormacaoAcademicaemAndamento = FormacaoAcademicaemAndamento;
+            dadosViewModel.CNPJdaEscola = CNPJdaEscola;
+            dadosViewModel.Previsaodeconcluçao = Previsaodeconcluçao;
+            dadosViewModel.DDD = DDD;
+            dadosViewModel.Telefone = Telefone;
+            dadosViewModel.Email = Email;
+            
+
+            return View(dadosViewModel);
+        }
+    }
+}
---
+++ Web/Models/Home/DadosViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models.Home
+{
+    public class DadosViewModel
+    {
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public int Cpf { get; set; }
+
+        public string EstadoCivil { get; set; }
+
+        public string NomeDeTratamento { get; set; }
+
+        public int DocumentoDeIdentidade { get; set; }
+
+        public string Sexo { get; set; }
+
+        public string Escolaridade { get; set; }
+
+        public string FormaçãoAcademica { get; set; }
+
+        public DateTime ConclucaoMmesAno { get; set; }
+
+        public string FormacaoAcademicaemAndamento { get; set; }
+
+        public int CNPJdaEscola { get; set; }
+
+        public DateTime Previsaodeconcluçao { get; set; }
+
+        public int DDD { get; set; }
+
+        public int Telefone { get; set; }
+
+        public string Email { get; set; }
+    }
+
+}
